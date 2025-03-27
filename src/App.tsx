a8d6ddeaf573b096fccf1fcc7c47bb4bc20f950c
Deepanshu954 <137024426+Deepanshu954@users.noreply.github.com>
2025-03-27 20:55:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import Index from "./pages/Index";
 import StateDetail from "./pages/StateDetail";
 import Admin from "./pages/Admin";
@@ -12,6 +13,8 @@ import AllStates from "./pages/AllStates";
 import Cuisine from "./pages/Cuisine";
 import Culture from "./pages/Culture";
 import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import AccountSettings from "./pages/AccountSettings";
 import JourneyDetail from "./pages/JourneyDetail";
 import { useEffect } from "react";
 import { ThemeProvider } from "./components/theme/ThemeProvider";
@@ -27,24 +30,28 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-        <TooltipProvider>
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/states" element={<AllStates />} />
-              <Route path="/state/:stateId" element={<StateDetail />} />
-              <Route path="/cuisine" element={<Cuisine />} />
-              <Route path="/culture" element={<Culture />} />
-              <Route path="/journey/:journeyId" element={<JourneyDetail />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/admin" element={<Admin />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </TooltipProvider>
+        <AuthProvider>
+          <TooltipProvider>
+            <Toaster />
+            <Sonner />
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/states" element={<AllStates />} />
+                <Route path="/state/:stateId" element={<StateDetail />} />
+                <Route path="/cuisine" element={<Cuisine />} />
+                <Route path="/culture" element={<Culture />} />
+                <Route path="/journey/:journeyId" element={<JourneyDetail />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/account-settings" element={<AccountSettings />} />
+                <Route path="/admin" element={<Admin />} />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </BrowserRouter>
+          </TooltipProvider>
+        </AuthProvider>
       </ThemeProvider>
     </QueryClientProvider>
   );
